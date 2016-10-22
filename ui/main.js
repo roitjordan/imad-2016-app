fb27bf2b1144e082f7f1f3c847725136798e589c
roitjordan <rohitkumarsahuu@gmail.com>
2016-10-22 15:21:41 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,6 +1,10 @@
 console.log('Loaded!');
 var img=document.getElementById('madi');
-img.onclick=function()
-{
-    img.style.marginLeft='100px';
+var marginLeft=0;
+function moveRight(){
+    marginLeft=marginLeft+10;
+    img.style.marginLeft=marginLeft +'px';
+}
+img.onclick=function(){
+    var interval=setInterval(moveRight,100);
 };
